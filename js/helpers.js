466e6c28080f148f093e1eb4f1bbe778f2e5c621
joanlopez <joanjan14@gmail.com>
2015-07-21 10:14:36 +0200

    Added new helper

--- js/helpers.js
+++ js/helpers.js
@@ -2,6 +2,15 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function getRandomIntsArray(n, min, max) {
+	var randomInts = [];
+	for(var i = 0; i < n; ++i) {
+		var random = getRandomInt(min, max);
+		randomInts.push(random);
+	}
+	return randomInts;		
+}
+
 function getArrayRandomItem(items) {
 	var random = getRandomInt(0, items.length-1);
 	return items[random];		
